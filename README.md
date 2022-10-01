41f1e2ca83c7c4c3fa18633019b6e378faa19629
34j <55338215+34j@users.noreply.github.com>
2022-10-01 20:50:33 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,70 +1,25 @@
-# Chrome Extension TypeScript Starter
+# Translate and Search (Chrome Extension)
 
-![build](https://github.com/chibat/chrome-extension-typescript-starter/workflows/build/badge.svg)
+![GitHub](https://img.shields.io/github/license/34j/chrome-extension-translate-search)
 
-Chrome Extension, TypeScript and Visual Studio Code
+A browser extension that when navigating to a url that contains the parameter `q`
 
-## Prerequisites
+- blocks navigating
+- then translates the content of the parameter `q`
+- then navigates back to the modified url.
 
-* [node + npm](https://nodejs.org/) (Current Version)
+## Tested
 
-## Option
+- Google Search
+- Google Scholar
+- DuckDuckGo
 
-* [Visual Studio Code](https://code.visualstudio.com/)
+## 日本語の説明
 
-## Includes the following
+パラメータ`q`を含むurlにナビゲートしたときに、ナビゲートをブロックし、パラメータ`q`の内容を翻訳し、再びナビゲートするブラウザ拡張機能です。
 
-* TypeScript
-* Webpack
-* React
-* Jest
-* Example Code
-    * Chrome Storage
-    * Options Version 2
-    * content script
-    * count up badge number
-    * background
+## 動作確認済み
 
-## Project Structure
-
-* src/typescript: TypeScript source files
-* src/assets: static files
-* dist: Chrome Extension directory
-* dist/js: Generated JavaScript files
-
-## Setup
-
-```
-npm install
-```
-
-## Import as Visual Studio Code project
-
-...
-
-## Build
-
-```
-npm run build
-```
-
-## Build in watch mode
-
-### terminal
-
-```
-npm run watch
-```
-
-### Visual Studio Code
-
-Run watch mode.
-
-type `Ctrl + Shift + B`
-
-## Load extension to chrome
-
-Load `dist` directory
-
-## Test
-`npx jest` or `npm run test`
+- Google Search
+- Google Scholar
+- DuckDuckGo
--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,85 @@
-function polling() {
-  // console.log("polling");
-  setTimeout(polling, 1000 * 30);
+import translate from 'google-translate-api-x'
+
+//Check, change, show the function is activated or not.
+let activated = true;
+
+function showActivated() {
+  chrome.action.setIcon({
+    path: activated ? "/icon_normal.png" : "/icon_inactive.png"
+  })
+  chrome.action.setBadgeText({ text: activated ? "ON" : "OFF" })
 }
 
-polling();
+chrome.storage.local.get("activated", items => {
+  activated = items.activated
+  showActivated();
+})
+
+
+chrome.action.onClicked.addListener(e => {
+  activated = !activated
+  chrome.storage.local.set({ "activated": activated })
+  showActivated()
+})
+
+//https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webNavigation
+//onBeforeNavigate does not work on some websites like twitter.com.
+//onBeforeNavigate prevents loading but onHistoryStateUpdated does not.
+//Using onHistoryStateUpdated only in such websites will be nice.
+chrome.webNavigation.onBeforeNavigate.addListener(
+  details => {
+    if (!details.url.includes('translated=true')
+      && !details.url.includes("uviewer")
+      && activated
+    ) {
+      chrome.tabs.update(details.tabId, { url: "" }).then(async _ => {
+        await translateUrl(details.url).then(url => chrome.tabs.update(details.tabId, { url }))
+      }
+      )
+    }
+  },
+  {
+    url: [
+      { urlMatches: '[?&]q=' }
+    ]
+  }
+)
+
+/*
+//Cannot use async/await here
+chrome.webRequest.addListner(async details => {
+  return { redirectUrl: await translateUrl(details.url) }
+})
+//This causes many problems because blocks every requests (not only browser navigation).
+chrome.webRequest.addListner(details => {
+  return { cancel: true }
+})
+//Better to use webNavigation.onBeforeNavigate than using tabs.onUpdated
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'loading'
+    && tab.url?.match('[?&]q=')
+    && !tab.url.includes('translated=true')) {
+    translateUrl(tab.url).then(url => chrome.tabs.update(tabId, { url }))
+  }
+});
+*/
+
+//translate apis.
+async function translateUrl(url: string) {
+  const u = new URL(url)
+  const q = u.searchParams.get('q')
+  if (q) {
+    u.searchParams.set('q', await translateText(q))
+  }
+  u.searchParams.set('translated', 'true')
+  return u.toString()
+}
+
+async function translateText(text: string) {
+  const res = await translate(text, { to: 'en' });
+  if ('text' in res && typeof res.text === 'string') {
+    return res.text
+  } else {
+    throw new Error('Translation type check failed');
+  }
+}
